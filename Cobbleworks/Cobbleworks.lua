3d30457fd976e05acd8da3046d0a358851350e29
iTitus <iTitus@users.noreply.github.com>
2017-08-20 22:48:26 +0200

    Print the stored amount

--- Cobbleworks/Cobbleworks.lua
+++ Cobbleworks/Cobbleworks.lua
@@ -1,4 +1,16 @@
 pipe = peripheral.wrap("bottom")
 lp = pipe.getLP()
 
-print("Hello World")
+materials = {
+  "cobble"] = {
+    ["id"] = function() return lp.getItemIdentifierBuilder().setItemID("minecraft:cobblestone").build() end
+  }
+}
+
+function getItemAmount(material)
+  return pipe.getItemAmount(materials[material]["id"]())
+end
+
+for material, data in materials do
+  print(material, getItemAmount(material))
+end
