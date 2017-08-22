1907f73ccf3b3412c259bdcd3fba0545514563af
iTitus <iTitus@users.noreply.github.com>
2017-08-22 14:11:13 +0200

    Flesh out table layout and add isEnabled function

--- Cobbleworks/Cobbleworks.lua
+++ Cobbleworks/Cobbleworks.lua
@@ -1,21 +1,48 @@
 pipe = peripheral.wrap("bottom")
 lp = pipe.getLP()
 
+MAX_TRESHOLD = 2048
+INGREDIENT_TRESHOLD = MAX_TRESHOLD / 2
+
 materials = {
-  ["cobble"] = {
-    ["id"] =
-	  function()
-	    builder = lp.getItemIdentifierBuilder()
-	    builder.setItemID("minecraft:cobblestone")
-	    return builder.build()
-	  end
+  cobble = {
+    id = "minecraft:cobblestone",
+	data = 0,
+	force = true
+  },
+  stone = {
+    id = "minecraft:stone",
+	data = 0,
+	ingredient = "cobble"
   }
 }
 
+function getItemIdentifier(material)
+  local data = materials[material]
+  local builder = lp.getItemIdentifierBuilder()
+  builder.setItemID(data.id)
+  builder.setItemData(data.data)
+  return builder.build()
+end
+
 function getItemAmount(material)
-  return pipe.getItemAmount(materials[material]["id"]())
+  return pipe.getItemAmount(getItemIdentifier(material))
+end
+
+function isEnabled(material)
+  local data = materials[material]
+  if data.force ~= nil then
+    return data.force
+  end
+  local ingredient = data.ingredient
+  if ingredient ~= nil then
+    if getItemAmount(ingredient) < (data.ingredientTreshold or INGREDIENT_TRESHOLD) then
+	  return false
+	end
+  end
+  return getItemAmount(material) >= (data.maxTreshold or MAX_TRESHOLD)
 end
 
 for material, data in pairs(materials) do
-  print(material, getItemAmount(material))
+  print(material..": "..getItemAmount(material).." -> "..isEnabled(material))
 end
