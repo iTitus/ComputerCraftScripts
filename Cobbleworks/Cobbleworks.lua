2c73f2a2af4abfdb94899e7929524ad758b5aa78
iTitus <iTitus@users.noreply.github.com>
2017-08-22 15:44:55 +0200

    Start on colors

--- Cobbleworks/Cobbleworks.lua
+++ Cobbleworks/Cobbleworks.lua
@@ -8,51 +8,66 @@ materials = {
   cobble = {
     id = "minecraft:cobblestone",
     data = 0,
-    force = true
+    force = true,
+    color = colors.red
   },
   stone = {
     id = "minecraft:stone",
-    data = 0
+    data = 0,
+    color = colors.green
   },
   sand = {
     id = "minecraft:sand",
     data = 0,
-    ingredient = "cobble"
+    ingredient = "cobble",
+    color = colors.brown
   },
   slag = {
     id = "ThermalExpansion:material",
     data = 514,
-    ingredients = {"sand", "cobble"}
+    ingredients = {"sand", "cobble"},
+    color = colors.blue
   },
   gravel = {
     id = "minecraft:gravel",
     data = 0,
-    ingredient = "stone"
+    ingredient = "stone",
+    color = colors.purple
   },
   flint = {
     id = "minecraft:flint",
     data = 0,
-    ingredient = "gravel"
+    ingredient = "gravel",
+    color = colors.cyan
   },
   silicon = {
     id = "EnderIO:itemMaterial",
     data = 0,
-    ingredient = "sand"
+    ingredient = "sand",
+    color = colors.lightGray
   },
   glass = {
     id = "minecraft:glass",
     data = 0,
-    ingredient = "sand"
+    ingredient = "sand",
+    color = colors.gray
   },
   niter = {
     id = "ThermalFoundation:material",
     data = 17,
-    ingredient = "sand"
+    ingredient = "sand",
+    color = colors.pink
   },
   richSlag = {
     id = "ThermalExpansion:material",
     data = 515,
-    ingredient = "redstone"
+    ingredient = "redstone",
+    color = colors.lime
+  },
+  obsidian = {
+    id = "minecraft:obsidian",
+    data = 0,
+    color = colors.yellow
   },
   redstone = {
     id = "minecraft:redstone",
@@ -87,6 +102,18 @@ function isEnabled(material)
   return getItemAmount(material) < MAX_TRESHOLD
 end
 
-for material, data in pairs(materials) do
-  print(material..": "..getItemAmount(material).." -> "..tostring(isEnabled(material)))
+while true do
+  for material, data in pairs(materials) do
+    local text = material..": "..getItemAmount(material).." -> "
+	term.write(text)
+	
+	local color = data.color
+	if color ~= nil then
+	  local state = isEnabled(material)
+	  term.blit(tostring(state), state and colors.green or colors.red, colors.black)
+	  term.blit("    ", color, color)
+	end
+  end
+  sleep(1)
+  term.clear()
 end
