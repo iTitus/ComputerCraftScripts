a0d0ee03477dfc82a4d94471d7323698e0cc1573
iTitus <iTitus@users.noreply.github.com>
2017-08-22 15:51:33 +0200

    Fix indentation

--- Cobbleworks/Cobbleworks.lua
+++ Cobbleworks/Cobbleworks.lua
@@ -105,13 +105,13 @@ end
 while true do
   for material, data in pairs(materials) do
     local text = material..": "..getItemAmount(material).." -> "
-	term.write(text)
-	
-	local color = data.color
-	if color ~= nil then
-	  local state = isEnabled(material)
-	  term.blit(state and "v" or "x", state and "5" or "e", "f")
-	end
+    term.write(text)
+
+    local color = data.color
+    if color ~= nil then
+      local state = isEnabled(material)
+      term.blit(state and "v" or "x", state and "5" or "e", "f")
+    end
   end
   sleep(1)
   term.clear()
