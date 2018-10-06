32850289b0854746fd883d576250c010c1f27d91
iTitus <iTitus@users.noreply.github.com>
2018-10-06 13:08:11 +0200

    Fix

--- StarlightTransmutation/starlight_transmutation.lua
+++ StarlightTransmutation/starlight_transmutation.lua
@@ -5,11 +5,13 @@ local os   = require("os")
 local r    = require("robot")
 local s    = require("sides")
 local n    = com.navigation
+local inv  = com.inventory_controller
 
 local HOME            = { facing=s.west, x=-15.5, y=130.5, z=52.5 }
-local INPUT           = s.south
-local OUTPUT          = s.east
+local INPUT           = { facing=s.south, name="tile.sandstone.name", damage=0 }
+local OUTPUT          = { facings.east, name="tile.end_stone.name", damage=0 }
 local ENERGY_TRESHOLD = 0.99
+local SIZE            = 3
 local WORK            = {}
 
 ------------------------------
@@ -18,11 +20,10 @@ function fill_work()
   -- assuming HOME is in the south-east corner and
      -- one block below the 3x3 work area
   local y = HOME.y + 1
-  local size = 3
-  for z = 0, 1 - size, -1 do
+  for z = 0, 1 - SIZE, -1 do
     local odd   = i % 2 == 1
-    local start = if odd then  0        else 1 - size
-    local end_  = if odd then  1 - size else 0
+    local start = if odd then  0        else 1 - SIZE
+    local end_  = if odd then  1 - SIZE else 0
     local step  = if odd then -1        else 1
     for x = start, end_, step do
 	  table.insert(WORK, { x=x, y=y, z=z })
@@ -44,9 +45,10 @@ function rotate_to(side)
 end
 
 function move_forward(n)
-  for i = 1, 10, 1 do
+  for i = 1, n, 1 do
     while not r.forward() do
-	  os.sleep(0.1)
+	  print("Cannot move: path obstructed!")
+	  os.sleep(0.25)
 	end
   end
 end
@@ -73,45 +75,89 @@ end
 function go_home()
   local x, y, z = n.getPosition()
   go_to(x, y, z)
+  rotate_to(HOME.facing)
 end
 
 function prep_inv()
-  rotate_to(OUTPUT)
+  rotate_to(OUTPUT.facing)
+  
   for i = 2, 15, 1 do
     r.select(i)
     r.drop()
   end
+  
   r.select(16)
   local count = r.count()
   if count > 1
     r.drop(count - 1)
   end
-  rotate_to(INPUT)
+  -----------------------------------------------------------------------------
+  rotate_to(INPUT.facing)
+  
   r.select(1)
-  r.suck()
+  local space = r.space()
+  if space > 0 then
+    for i = 1, inv.getInventorySize(s.front), 1 do
+      local item = inv.getStackInSlot(s.front, i)
+	  if item and item.name == INPUT.name and item.damage == INPUT.damage then
+	    if inv.suckFromSlot(s.front, i, space) then
+	      space = r.space()
+	    end
+	    if space == 0 then
+	      break
+	    end
+	  end
+    end
+  end
+  
+  for i = 2, 15, 1 do
+    r.select(i)
+    r.drop()
+  end
+  -----------------------------------------------------------------------------
+  rotate_to(HOME.facing)
 end
 
 function not_ready()
   if comp.energy() < ENERGY_TRESHOLD * comp.maxEnergy() then
+    print("Missing energy")
     return true
   end
-  if r.count(1) < 9 do
+  local facing = n.getFacing()
+  if facing ~= HOME.facing then
+    print("Wrong facing")
+    return false
+  end
+  local x, y, z = n.getPosition()
+  if x ~= HOME.x or y ~= HOME.y or z ~= HOME.z then
+    print("Wrong position")
+    return false
+  end
+  local stack = r.getStackInInternalSlot(1)
+  if not stack or stack.size < min(SIZE * SIZE + 1, stack.maxSize) or stack.name ~= INPUT.name or stack.damage ~= INPUT.damage then
+    print("Stack in Slot 1 does not equal INPUT with minimum size " .. min(SIZE * SIZE + 1, stack.maxSize))
     return true
   end
   for i = 2, 15, 1 do
     if r.count(i) > 0 do
+	  print("Stack in Slot " .. i .. " is not empty")
 	  return true
 	end
   end
-  if r.count(16) ~= 1 do
+  stack = r.getStackInInternalSlot(16)
+  if not stack or stack.size ~= 1 or stack.name ~= OUTPUT.name or stack.damage ~= OUTPUT.damage then
+    print("Stack in Slot 16 does not equal OUTPUT with size 1")
     return true
   end
+  
   return false
 end
 
 function wait_until_ready()
   while not_ready() do
-    os.sleep(1)
+    os.sleep(5)
+	go_home()
+	prep_inv()
   end
 end
 
@@ -128,6 +174,9 @@ function work()
   for _, pos in ipairs(WORK) do
     go_to(pos.x, pos.y, pos.z)
 	do_work()
+	if r.count(1) <= 1 then
+	  break
+	end
   end
 end
 
