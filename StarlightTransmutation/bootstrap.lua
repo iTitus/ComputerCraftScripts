42ba46268faa30c5db538170526084533f2cc770
iTitus <iTitus@users.noreply.github.com>
2018-10-06 13:23:08 +0200

    Add rebooting back in - derp

--- StarlightTransmutation/bootstrap.lua
+++ StarlightTransmutation/bootstrap.lua
@@ -1,6 +1,29 @@
 local shell = require("shell")
+local os = require("os")
 
-print("Updating files...")
-shell.execute("update")
-print("Starting Starlight Transmutation")
-shell.execute("starlight_transmutation")
+function setState(state)
+  file = io.open("state", "w")
+  file:write(state)
+  file:close()
+end
+ 
+function getState()
+  local state = "0"
+  file = io.open("state", "r")
+  if file then
+    state = file:read("*a")
+    file:close()
+  end
+  return state
+end
+
+if getState() == "0" then
+  print("Updating files...")
+  shell.execute("update")
+  setState("1")
+  os.shutdown(true)
+else
+  print("Starting Starlight Transmutation")
+  setState("0")
+  shell.execute("starlight_transmutation")
+end
